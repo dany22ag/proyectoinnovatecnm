db1788541bcdb3b5b8c272e7e94cb51f61f2f36a
dany22ag <danyaguilar2003@outlook.com>
2025-05-03 19:24:26 -0500

    segundo commit

---
+++ src/component.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+export default function Component() {
+    const [text, setText] = useState()
+    const textOnChange = (event) => {
+        setText(event.target.value)
+    }
+    return (
+    <div>
+        <input type="text" value={text} onChange={textOnChange}/>
+        <button>Actualizar</button>
+        <p>Texto input: {text}</p>
+        <p>Texto actualizado</p>
+
+    </div>
+  )
+}
